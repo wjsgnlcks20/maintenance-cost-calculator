7ab73335ebc613324f5fa5242ec8a9c874a6bb65
wjsgnlcks20 <gnlcks82@gmail.com>
2022-07-10 23:20:26 +0900

    compeleting core functions and adding features / V2

--- excel_automation_demo.py
+++ excel_automation_demo.py
@@ -1,35 +1,28 @@
 import xlrd
 import xlwt
-from maintenance_cost_calculator import maintenance_cost_calculator
+from maintenance_cost_calculator import get_output
 
 DATA_FILENAME = "데이터.xls"
 PRINT_FILENAME = "소액관리비분류완료.xls"
-BUILDING_DEFAULT_COST_FILENAME = "건물기본비용.xls"
+
+FREE_PASS_FILENAME = "관리비비용청구불필요.xls"
+EXCEPTION_FILENAME = "수리비기준.xls"
+DEFAULT_COST_FILENAME = "기본비용.xls"
+
 DATA_PRINT_COL_GAP = 1
 
 # 입력 엑셀 속성 이름
-COST_ATT = "입금"
-BUILDING_ATT = "건물이름"
-MAXIMUM_CALCULATE_COST = 150000
+COST_ATT = "입금금액"
+NAME_ATT = "거래기록사항"
 
 # 출력 엑셀 속성 이름
-COST_CLASS = "비용 분류"
-MAINTENANCE_COST = "관리비"
-REMAIN_BALANCE = "그 외 비용"
+DEPOSIT_HISTORY = "입금내역"
+DEPOSIT_SPECIFIC = "입금상세내역"
+MAINTENANCE_COST = "수입금액"
+REMAIN_BALANCE = "수입제외금액"
 STATUS = "관리비 결정 방식"
 
-
-# 건물 기본값 받아오기
-def get_building_default_cost():
-    building_default_cost = {}
-
-    read_wb = xlrd.open_workbook(BUILDING_DEFAULT_COST_FILENAME)
-    sheet = read_wb.sheet_by_index(0)
-
-    for row in range(sheet.nrows):
-        building_default_cost[sheet.cell_value(row, 0)] = sheet.cell_value(row, 1)
-
-    return building_default_cost
+################################### 박제
 
 # 출력 파일의 기초값 설정 (기존 파일 값 복사하기)
 # 새로운 값을 작성할 수 있는 최소 col 값 반환.
@@ -47,14 +40,14 @@ def get_add_col_start(minimum_col_start):
 # 출력 파일 속성 작성
 def add_col_to_write_file(write_file, col_name, col_start):
     for i in range(len(col_name)):
-        write_file.write(0, col_start + i, col_name[i])
+        write_file.write(7, col_start + i, col_name[i])
 
 # 출력 파일의 속성을 가져온다(데이터 파일 속성 + 추가 속성)
 def get_added_col(read_file, add_col_names, col_start):
     col_names = []
     for i in range(col_start):
         if i < read_file.ncols:
-            col_names.append(read_file.cell_value(0, i))
+            col_names.append(read_file.cell_value(7, i))
         else:
             col_names.append(f"blank{i}")
     col_names += add_col_names
@@ -66,50 +59,94 @@ def index_col(col_names):
     for i, col_name in enumerate(col_names):
         indexing[col_name] = i
     return indexing
+################################### 박제
+
+# 데이터로 활용해야 할 파일들을 열어서 데이터 화 해둔 뒤,
+# 각 열마다 데이터를 처리하려고 할 때 파라미터로 같이 보내면 메모리와 시간을 절약.
+
+def get_default_cost_data():
+    default_cost = {}
+
+    read_wb = xlrd.open_workbook(DEFAULT_COST_FILENAME)
+    sheet = read_wb.sheet_by_index(0)
+
+    for row in range(sheet.nrows):
+        default_cost[sheet.cell_value(row, 0)] = sheet.cell_value(row, 1)
+
+    return default_cost
+
+def get_free_pass_data():
+    wb = xlrd.open_workbook(FREE_PASS_FILENAME)
+    read_file = wb.sheet_by_index(0)
+
+    free_pass_data = {}
+    for row in range(1, read_file.nrows):
+        deposit_specific = str(read_file.cell_value(row, 0))
+        name = str(read_file.cell_value(row, 1))
+        free_pass_data[deposit_specific + name] = (read_file.cell_value(row, 2), read_file.cell_value(row, 3))
+
+    return free_pass_data
+
+def get_exception_data():
+    wb = xlrd.open_workbook(EXCEPTION_FILENAME)
+    read_file = wb.sheet_by_index(0)
+
+    exception_data = {}
+    for row in range(1, read_file.nrows):
+        exception_data[read_file.cell_value(row, 0)] = (read_file.cell_value(row, 1), read_file.cell_value(row, 2), read_file.cell_value(row, 3))
+    return exception_data
 
 # 프로그램 실행
 def process_maintenance_data():
     # 파일에서 건물 기본값, 계산초과기준비용 가져오기
-    building_default_cost = get_building_default_cost()
-    maximum_calculate_cost = MAXIMUM_CALCULATE_COST
 
     # 데이터 및 출력 파일 불러오기
     read_wb = xlrd.open_workbook(DATA_FILENAME)
     read_ws = read_wb.sheet_by_index(0)
 
     write_wb = xlwt.Workbook(encoding="utf-8")
-    write_ws = write_wb.add_sheet("sheet1")
+    write_ws = write_wb.add_sheet("sheet1", cell_overwrite_ok=True)
 
     # 출력 파일 초기화 및 col 설정
     minimum_col_start = init_write_file(write_ws, read_ws)
     add_col_start = get_add_col_start(minimum_col_start)
 
-    add_col_names = [ COST_CLASS, MAINTENANCE_COST, REMAIN_BALANCE, STATUS ]
+    add_col_names = [ STATUS ]
     add_col_to_write_file(write_ws, add_col_names, add_col_start)
     col_names = get_added_col(read_ws, add_col_names, add_col_start)
 
     # 파일 작성 시 col 값 인덱싱
     indexing = index_col(col_names)
 
-    # 각 데이터 별 처리
-    for cur_row in range(1, read_ws.nrows):
+    # 데이터 처리 시 필요한 파일 데이터 가공
+    free_pass_data = get_free_pass_data()
+    exception_data = get_exception_data()
+    default_cost_data = get_default_cost_data()
 
-        # 입금내역과 건물이름을 가져온다
+    # 각 데이터 별 처리
+    for cur_row in range(8, read_ws.nrows - 1):
+        # 입력된 데이터가 있는 경우 계산하지 않는다.
+        if read_ws.cell_value(cur_row, indexing[REMAIN_BALANCE]):
+            continue
+
+        # 입금내역과 거래기록사항을 가져온다
         cost = read_ws.cell_value(cur_row, indexing[COST_ATT])
-        building_name = read_ws.cell_value(cur_row, indexing[BUILDING_ATT])
+        name = str(read_ws.cell_value(cur_row, indexing[NAME_ATT]))
 
         # 관리비와 상태(계산방식 등)을 구한다
-        maintenance_cost, status = maintenance_cost_calculator(cost, building_name, building_default_cost, maximum_calculate_cost)
+        deposit_history, deposit_specific, maintenance_cost, status = get_output(cost, name, free_pass_data, exception_data, default_cost_data)
 
-        # 셀이 빈 ""이 int 처리되어야 하는 경우에 대해서 조금 더 깔끔하게 처리해보고 싶은데...
-        write_ws.write(cur_row, indexing[STATUS], status)
-        if maintenance_cost == "":
+        # 파일에 작성
+        try:
+            write_ws.write(cur_row, indexing[STATUS], status)
+            write_ws.write(cur_row, indexing[DEPOSIT_HISTORY], deposit_history)
+            write_ws.write(cur_row, indexing[DEPOSIT_SPECIFIC], deposit_specific)
+            write_ws.write(cur_row, indexing[MAINTENANCE_COST], maintenance_cost)
+            write_ws.write(cur_row, indexing[REMAIN_BALANCE], int(cost) - maintenance_cost)
+        except:
             continue
-        write_ws.write(cur_row, indexing[COST_CLASS], "관리비")
-        write_ws.write(cur_row, indexing[MAINTENANCE_COST], maintenance_cost)
-        write_ws.write(cur_row, indexing[REMAIN_BALANCE], int(cost) - maintenance_cost)
-        # 남은 금액 음수 나오는 경우 확인
-        if int(cost) - maintenance_cost < 0:
+
+        if int(cost) < maintenance_cost:
             print(f"on row num: {cur_row}, there's a negative balance!")
 
     # 파일 저장
--- maintenance_cost_calculator.py
+++ maintenance_cost_calculator.py
@@ -1,32 +1,130 @@
+
+MAXIMUM_MOVING_COST = 300000
+MAXIMUM_MAINTENANCE_COST = 160000
+MAXIMUM_CALCULATE_COST = 100000
+DIVIDE_BY = 1000
+OVER_CALCULATION_MAINTENANCE_COST = 26000
+
+# 이름에 호수가 포함된 경우 저장된 이름으로 변경해준다.
+def get_labeled_name(input_name, labeled_names):
+    for labeled_name in labeled_names:
+        if input_name.find(labeled_name) >= 0:
+            print(f"found labeled name! for {input_name} -> {labeled_name}")
+            return labeled_name
+    return "no_labeled_name"
+
+# 입금상세내역 주택, 집단 오피스텔 확인
+def get_deposit_specific(name):
+     if name.find("크리스탈") >= 0 or name.find("야베스") >= 0:
+        return "집단 오피스텔"
+     else: return "주택"
+
 # 관리비 계산식
 def get_maintenance_cost(cost):
+    if cost // 10000 == 1:
+        return 12000
     return 10000 + (cost // 10000) * 1000
 
 def get_discounted_cost(cost):
     return round(float(cost) / 2, 0)
 
-# 입금 금액에 따라 최종 관리비와 그 외 비용을 결정함. 결정 상태도 반환.
-def maintenance_cost_calculator(cost, building_name, building_default_cost, maximum_calculate_cost):
-    # 입금이 아닌 출금인 경우 -> cost 가 비어있음. 손댈필요 x
-    if cost == "":
-        return "", "출금"
+# 3, 4번 기본값 받아오기
+def no_data_maintenance_cost_calculator(cost, name, default_cost):
 
     cost = int(cost)
-    # 계산초과기준금액 초과인 경우
-    if cost > maximum_calculate_cost:
-        return "", "기준금액 초과, 추후 계산 필요"
+    default_cost_names = []
+    for key in default_cost.keys():
+        default_cost_names.append(key)
 
-    default_cost = building_default_cost.get(building_name)
+    final_cost = default_cost.get(get_labeled_name(name, default_cost_names))
+    if get_deposit_specific(name) == "집단 오피스텔":
+        if not final_cost:
+            final_cost = get_maintenance_cost(cost)
+        if cost < final_cost:
+            final_cost = get_discounted_cost(cost)
+        return "관리비 수익", get_deposit_specific(name), final_cost, "집단 오피스텔"
+
+    # 계산초과기준금액 초과인 경우
+    if cost > MAXIMUM_MOVING_COST:
+        return "", "", "", "계산 기준 금액 초과, 추후 계산"
+    elif MAXIMUM_MAINTENANCE_COST <= cost < MAXIMUM_MOVING_COST:
+        return "대납비", "이사정산", 0, "이사정산"
+    elif cost % DIVIDE_BY == 0:
+        return "대납비", "수리비", 0, "깔끔하게 떨어지는 수리비"
+    elif MAXIMUM_CALCULATE_COST <= cost < MAXIMUM_MAINTENANCE_COST:
+        return "관리비 수익", get_deposit_specific(name), OVER_CALCULATION_MAINTENANCE_COST, "10만 이상 관리비"
+
     # 건물 관리 비용 없음, 계산식에 의한 계산
-    if not default_cost:
-        default_cost = get_maintenance_cost(cost)
+    if not final_cost:
+        final_cost = get_maintenance_cost(cost)
         status = "건물 기본 등록비용 없음. 계산에 의한 관리비"
-    # 건물 관리 비용 존재
     else:
         status = "건물 기본 등록비용"
 
-    # 입금 금액보다 비용이 큰 경우 입금 금액의 50% 청구
-    if cost < default_cost:
-        return get_discounted_cost(cost), "건물 기본 등록비용 미만, 수정된 가격"
+    # 건물 관리 비용 존재, 보다 아래인 경우 50%
+    if cost < final_cost:
+        final_cost = get_discounted_cost(cost)
+        status = "건물 기본 등록비용 미만, 수정된 가격"
 
-    return default_cost, status
+    # 건물 관리 비용 존재, 보다 이상인 경우
+    return "관리비 수익", get_deposit_specific(name), final_cost, status
+
+# 1번 관리비비용청구 불필요인 경우
+def free_pass_calculator(name, free_pass_data):
+    labeled_name = []
+    for key in free_pass_data.keys():
+        labeled_name.append(key)
+    name = get_labeled_name(name, labeled_name)
+
+    deposit_data = free_pass_data.get(name)
+    if not deposit_data:
+        return "", ""
+    return deposit_data[0], deposit_data[1]
+
+# 2번 특수 경우 처리
+def exception_calculator(cost, name, exception_data):
+    labeled_name = []
+    for key in exception_data.keys():
+        labeled_name.append(key)
+    name = get_labeled_name(name, labeled_name)
+    if name == "no_labeled_name":
+        return "", "", "", ""
+
+    if name == "정성용" or name == "이해권":
+        if cost < int(exception_data[name][2]):
+            return "대납액", "수리비", 0, "대납액 수리비 처리"
+        else:
+            return "", "", "", "계산 기준 금액 초과, 추후 처리"
+    if name == "이석재":
+        if cost == int(exception_data[name][2]):
+            return exception_data[name][0], get_deposit_specific(name), exception_data[name][2], "관리비 수입 처리"
+        else :
+            return "대납액", "수리비", 0, "대납액 수리비 처리"
+
+    if exception_data[name][0] == "대납액":
+        return exception_data[name][0], exception_data[name][1], 0, "대납액 수리비 처리"
+    else:
+        if cost < int(exception_data[name][2]):
+            return "대납액", "수리비", 0, "대납액 수리비 처리"
+        else:
+            return exception_data[name][0], get_deposit_specific(name), exception_data[name][2], "관리비 수입 처리"
+
+# 새로운 maintenance_calculator
+def get_output(cost, name, free_pass_data, exception_data, default_cost_data):
+
+    # 입금이 아닌 출금인 경우 -> cost 가 비어있음. 손댈필요 x
+    if cost == "":
+        return "", "", "", "출금"
+
+    # 1. 관리비비용청구 불필요
+    deposit_history, deposit_specific = free_pass_calculator(name, free_pass_data)
+    if deposit_history != "":
+        return deposit_history, deposit_specific, 0, "기신고 및 수리비(관리비청구불필요)"
+
+    # 2. 특수 경우 처리(수리비 판단 여부 기준, 특수 경우 예외처리)
+    deposit_history, deposit_specific, final_cost, status = exception_calculator(cost, name, exception_data)
+    if deposit_history != "":
+        return deposit_history, deposit_specific, final_cost, status
+
+    # 3, 4. 기본값 확인 후 없는경우 계산식으로 처리.
+    return no_data_maintenance_cost_calculator(cost, name, default_cost_data)
